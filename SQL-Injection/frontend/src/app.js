fe2429f802edfdfeaed2b026d8d2242e83f0302c
Wei Chi Lin <weichilin06@gmail.com>
2019-10-03 19:06:22 +0900

    Finished SQL-Injection

--- SQL-Injection/frontend/src/app.js
+++ SQL-Injection/frontend/src/app.js
@@ -3,6 +3,7 @@ const axios = require('axios');
 const app = express();
 var bodyParser = require('body-parser')
 var cookieParser = require('cookie-parser')
+var mysql = require('mysql')
 
 var path = require('path');
 var viewPath = path.join(__dirname, 'views');
@@ -16,14 +17,37 @@ app.use(express.static(publicPath));
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(cookieParser());
 
+//Set up MySQL Connection
+var connection = mysql.createConnection({
+    host     : 'localhost',
+    user     : 'root',
+    password : 'password',
+    database : 'sqlinject'
+  });
+
 
 app.get('/', (req, res) => {
     res.render('index');
 })
 
 app.post('/login', (req, res)=>{
-    const body = req.body
-    console.log(body)
+    const username = req.body.username
+    const password = req.body.password
+    let query = "SELECT * FROM credentials where username = '" + username + "' and password = '" + password + "'";
+    console.log("username: " + username);
+    console.log("password: " + password);
+    console.log('query: ' + query);
+    connection.query(query, function (error, results){
+        console.log(results)
+        console.log(error)
+        if (results.length){
+            res.send('FLAG_7483254367')
+        }else{
+        res.send('Wrong Credentials')
+        }
+    })
+    
+
 })
 
 app.listen(3030, ()=>{
